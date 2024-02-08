c8e064177f264d96b85c35a012a0c4d22bea6d65
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-08 07:58:25 +0100

    Update and rename install-ansile_aws-linux.sh to install-ansible_aws-linux.sh

---
+++ configurationManagementTools/Ansible/install-ansible_aws-linux.sh
@@ -0,0 +1,62 @@
+#!bin/bash
+
+###################### Install ansible from repository ################################
+
+## install 
+sudo yum install ansible  
+
+## check ansible version 
+ansible --version 
+
+############################ installation using python #################################3
+## Update system 
+sudo dnf update
+
+## Check if pip dans python is installed 
+pip --version
+pip3 --version
+python3 --version 
+
+## install python3 and pip 
+sudo dnf install python3.11
+sudo dnf install python3-pip ## or sudo dnf install  python3.11-pip
+
+## check if pip is installed 
+pip --verison 
+
+## intall ansible using pip 
+pip install ansible
+
+## check ansible version ]
+ansible --verison 
+
+
+####################### best installation from python command  ###########################
+
+## install python3 and pip 
+sudo dnf install python3.11
+sudo dnf install python3-pip ## or sudo dnf install  python3.11-pip
+
+## installation 
+python3 -m pip install --user ansible
+
+## You can install the minimal ansible-core package for the current user:
+python3 -m pip install --user ansible-core
+
+## Alternately, you can install a specific version of ansible-core:
+python3 -m pip install --user ansible-core==2.12.3
+
+## Upgrading Ansible
+## To upgrade an existing Ansible installation in this Python environment to the latest released version, simply add --upgrade to the command above:
+python3 -m pip install --upgrade --user ansible
+
+
+
+
+
+
+
+
+
+
+
