6858b809ebb28a0643a611a7e078be071efbf71d
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-10 13:34:52 +0100

    Update install_aws_linux.sh

--- BuildAutomationTools/Gradle/install_aws_linux.sh
+++ BuildAutomationTools/Gradle/install_aws_linux.sh
@@ -1,1 +1,14 @@
 
+
+
+## install java 
+sudo yum install java-17-amazon-corretto-devel
+
+
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+
+## Install gradle 
+wget https://services.gradle.org/distributions/gradle-8.6-bin.zip
+
