eff89fd8c6dbcfdda12caa306f07cb39226a09f3
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-02-12 07:29:14 +0100

    Update and rename installation.sh to install-kub-awsLinux.sh

---
+++ Orchestration/install-kub-awsLinux.sh
@@ -0,0 +1,52 @@
+!#bin/bash
+
+#################################### install and config docker 
+
+ ### update system
+ sudo yum update 
+
+## install docker 
+sudo yum install docker 
+
+##  enable ans start docker 
+sudo systemctl enable docker 
+sudo systemctl start docker 
+sudo systemctl status docker
+
+
+################################# INSTALL KUBERNATES 
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# This overwrites any existing configuration in /etc/yum.repos.d/kubernetes.repo
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+### installation 
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+
+## restart : 
+sudo systemctl restart kubelet
+
+############################## INITIALIZE control plateform 
+#### 
+kubeadm init --apiserver-advertise-address=<master-private-IP> --pod-network-cidr= 
+
+
+
+
+
+
+
+
+
